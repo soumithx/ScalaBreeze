963e2ab5c8a4bdfca0e07443d35c71bf42f4c623
Sai Soumith Challa <soumithsaic@gmail.com>
2019-06-23 20:40:28 -0400

    Update Readme

--- PandasDataHandling/FeedData.py
+++ PandasDataHandling/FeedData.py
@@ -2,4 +2,4 @@ import pandas as pd
 InpData = pd.read_csv('DTCTLVENMNM.csv')
 FormatedInputData = pd.read_csv('DTCTLVENMNM.csv',sep=',',encoding='latin-1',nrows=5,skiprows=[2,3])
 print(type(FormatedInputData))
-
+print(FormatedInputData.shape(
