409c30f488578fe1113e70b64d324b12a7366c05
Sai Soumith Challa <soumithsaic@gmail.com>
2019-06-23 20:39:14 -0400

    Update Readme

--- PandasDataHandling/FeedData.py
+++ PandasDataHandling/FeedData.py
@@ -1,5 +1,5 @@
 import pandas as pd 
 InpData = pd.read_csv('DTCTLVENMNM.csv')
-FormatedInputData = pd.read_csv('DTCTLVENMM.csv',sep=',',encoding='latin-1',nrows=5,skiprows=[2,3])
+FormatedInputData = pd.read_csv('DTCTLVENMNM.csv',sep=',',encoding='latin-1',nrows=5,skiprows=[2,3])
 print(type(FormatedInputData))
 
